3f598a1cea2d7112a2ba47295564f3634111153c
rodrigues.tallyto <rodrigues.tallyto@hotmail.com>
2020-03-11 10:44:49 -0400

    Autenticando usuário

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const UserController = require("./app/controllers/UserController.js");
+const SessionController = require("./app/controllers/SessionController.js");
 const routes = new Router();
 
 routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
 
 module.exports = routes;
