fe96d7049788e91a9a123b5e41e7bd9c607bf619
rodrigues.tallyto <rodrigues.tallyto@hotmail.com>
2020-03-16 16:25:01 -0400

    Adicionado configuraçoes do servidor de aplicaçao

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'buddy@app123',
   database: 'buddypet',
   port: '5432',
   define: {
