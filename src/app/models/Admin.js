4d0b25c55ae64622860081a1434fafccba202abf
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2020-07-13 18:30:34 -0400

    feat: create admin model

---
+++ src/app/models/Admin.js
@@ -0,0 +1,34 @@
+const { Model } = require('sequelize');
+const Sequelize = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+class Admin extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+      },
+      { sequelize },
+    );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  static associate(models) {
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+module.exports = Admin;
