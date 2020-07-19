918591b6e5635051127c4a2719661e40fc3cbea9
Tallyto Rodrigues <rodrigues.tallyto@hotmail.com>
2020-07-19 02:47:56 -0400

    chore: added docker compose production

---
+++ docker.sh
@@ -0,0 +1,7 @@
+
+docker-machine create --driver amazonec2 buddy-api
+docker-machine env buddy-api
+eval $(docker-machine env buddy-api)
+docker-machine inspect buddy-api
+eval $(docker-machine env -u)
+
